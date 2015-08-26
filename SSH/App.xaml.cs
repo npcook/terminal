4f5aabf4a833af024f01786758cfbd4cd2a7aca0
npcook <nicholas.cook+github@outlook.com>
2015-08-26 14:19:20 -0400

    Change app startup to open ConnectionDialog first

--- SSH/App.xaml.cs
+++ SSH/App.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
@@ -13,5 +14,30 @@ namespace npcook.Ssh
 	/// </summary>
 	public partial class App : Application
 	{
+		public static new App Current
+		{ get { return Application.Current as App; } }
+
+		private void this_Startup(object sender, StartupEventArgs e)
+		{
+			ShutdownMode = ShutdownMode.OnExplicitShutdown;
+			var dialog = new ConnectionDialog();
+			if (dialog.ShowDialog().GetValueOrDefault(false))
+			{
+				var mainWindow = new MainWindow();
+				MainWindow = mainWindow;
+
+				var authList = new List<Authentication>();
+				if (dialog.Password != "")
+					authList.Add(new PasswordAuthentication(dialog.Password));
+				if (dialog.KeyFilePath != "")
+					authList.Add(new KeyAuthentication(File.Open(dialog.KeyFilePath, FileMode.Open), dialog.KeyFilePassphrase.ToString()));
+
+				mainWindow.Connect(dialog.ServerAddress, dialog.ServerPort, dialog.Username, authList);
+				mainWindow.Show();
+				ShutdownMode = ShutdownMode.OnLastWindowClose;
+			}
+			else
+				Shutdown();
+		}
 	}
 }
