e5fd3424a630afa1f483ea8f2729b004d58977ec
npcook <nicholas.cook+github@outlook.com>
2015-09-29 23:50:18 -0400

    Add tests

---
+++ TerminalControlTests/DequeTests.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace npcook.Terminal.Controls.Tests
+{
+	[TestClass]
+	public class DequeTests
+	{
+		[TestMethod]
+		public void TestInsert()
+		{
+			Deque<int> test = new Deque<int>(10);
+			test.Add(0);
+			test.Add(1);
+			test.Add(2);
+			test.Add(3);
+			test.Add(4);
+			test.Add(5);
+			test.Add(6);
+			test.Insert(3, 7);
+			test.Insert(3, 8);
+
+			CollectionAssert.AreEqual(test.ToArray(), new int[] { 0, 1, 2, 8, 7, 3, 4, 5, 6 });
+			Assert.AreEqual(test.Count, 9);
+		}
+
+		[TestMethod]
+		public void TestRemoveAt()
+		{
+			Deque<int> test = new Deque<int>(10);
+			test.Add(0);
+			test.Add(1);
+			test.Add(2);
+			test.Add(3);
+			test.Add(4);
+			test.Add(5);
+			test.Add(6);
+
+			test.RemoveAt(2);
+
+			CollectionAssert.AreEqual(test.ToArray(), new int[] { 0, 1, 3, 4, 5, 6 });
+			Assert.AreEqual(test.Count, 6);
+		}
+	}
+}
--- TerminalTest/TerminalLineTests.cs
+++ TerminalTest/TerminalLineTests.cs
@@ -11,20 +11,32 @@ namespace npcook.Terminal.Tests
 	[TestClass()]
 	public class TerminalLineTests
 	{
+		void AssertAreEqual(TerminalRun run1, TerminalRun run2)
+		{
+			Assert.AreEqual(run1.Text, run2.Text);
+			Assert.AreEqual(run1.Font, run2.Font);
+		}
+
 		[TestMethod()]
 		public void DeleteCharactersTest()
 		{
 			TerminalLine line = new TerminalLine();
-			line.SetCharacters(0, new string('a', 160), new TerminalFont() { Bold = true });
-			line.DeleteCharacters(70, 20);
+			line.SetCharacters(0, new string('0', 4), new TerminalFont() { Foreground = Color.FromRgb(0, 0, 0) });
+			line.SetCharacters(4, new string('1', 4), new TerminalFont() { Foreground = Color.FromRgb(1, 1, 1) });
+			line.SetCharacters(8, new string('2', 4), new TerminalFont() { Foreground = Color.FromRgb(2, 2, 2) });
+			line.SetCharacters(12, new string('3', 4), new TerminalFont() { Foreground = Color.FromRgb(3, 3, 3) });
+			line.DeleteCharacters(7, 6);
 
 			TerminalRun[] expectedRuns = new[]
 			{
-				new TerminalRun(new string('a', 70), new TerminalFont() {Bold = true }),
-				new TerminalRun(new string('a', 70), new TerminalFont() {Bold = true }),
+				new TerminalRun(new string('0', 4), new TerminalFont() { Foreground = Color.FromRgb(0, 0, 0) }),
+				new TerminalRun(new string('1', 3), new TerminalFont() { Foreground = Color.FromRgb(1, 1, 1) }),
+				new TerminalRun(new string('3', 3), new TerminalFont() { Foreground = Color.FromRgb(3, 3, 3) }),
 			};
 
-			Assert.Fail();
+			AssertAreEqual(line.Runs[0], expectedRuns[0]);
+			AssertAreEqual(line.Runs[1], expectedRuns[1]);
+			AssertAreEqual(line.Runs[2], expectedRuns[2]);
 		}
 	}
 }
