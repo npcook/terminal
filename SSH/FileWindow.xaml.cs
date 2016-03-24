cd77582d3ffaac8ac6dc24e575a243d361bdf3f4
npcook <nicholas.cook+github@outlook.com>
2016-03-23 23:02:10 -0400

    Add drag and drop

--- SSH/FileWindow.xaml.cs
+++ SSH/FileWindow.xaml.cs
@@ -18,9 +18,12 @@ using System.Collections.ObjectModel;
 
 namespace npcook.Ssh
 {
-	struct LocalFile
+	class LocalFile
 	{
 		public string Name
+		{ get; set; }
+
+		public string Path
 		{ get; set; }
 	}
 
@@ -41,6 +44,20 @@ namespace npcook.Ssh
 
 			localFilesList.ItemsSource = localFiles;
 			remoteFilesList.ItemsSource = remoteFiles;
+
+			watcher = new FileSystemWatcher()
+			{
+				Filter = "",	// Watch all files, no matter the extension
+				NotifyFilter = NotifyFilters.FileName | NotifyFilters.Size,
+				IncludeSubdirectories = false,
+			};
+
+			watcher.Created += Watcher_Created;
+		}
+
+		private void Watcher_Created(object sender, FileSystemEventArgs e)
+		{
+			throw new NotImplementedException();
 		}
 
 		class DirectoryComparer : IComparer<bool>
@@ -67,7 +84,10 @@ namespace npcook.Ssh
 		{
 			localPath = newPath;
 			foreach (var filename in Directory.GetFiles(localPath))
-				localFiles.Add(new LocalFile { Name = System.IO.Path.GetFileName(filename) });
+				localFiles.Add(new LocalFile { Name = System.IO.Path.GetFileName(filename), Path = filename });
+
+			watcher.Path = localPath;
+			watcher.EnableRaisingEvents = true;
 		}
 
 		private void changeRemoteDirectory(string newPath)
@@ -97,6 +117,87 @@ namespace npcook.Ssh
 			if (item != null && item.IsDirectory)
 			{
 				changeRemoteDirectory(item.FullName);
+			}
+		}
+
+		bool dragging = false;
+		Point dragStart;
+		LocalFile dragFile;
+		private void localFilesList_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+		{
+			dragging = true;
+			dragStart = e.GetPosition(localFilesList);
+			var itemContainer = FindAncestor<ListViewItem>(VisualTreeHelper.HitTest(localFilesList, dragStart).VisualHit);
+			dragFile = localFilesList.ItemContainerGenerator.ItemFromContainer(itemContainer) as LocalFile;
+			Mouse.Capture(localFilesList);
+
+			e.Handled = false;
+		}
+
+		static T FindAncestor<T>(DependencyObject current) where T : DependencyObject
+		{
+			do
+			{
+				if (current is T)
+					return (T) current;
+				current = VisualTreeHelper.GetParent(current);
+			} while (current != null);
+			return null;
+		}
+
+		private void localFilesList_PreviewMouseMove(object sender, MouseEventArgs e)
+		{
+			if (!dragging)
+				return;
+			var diff = dragStart - e.GetPosition(localFilesList);
+			if (Math.Abs(diff.X) < SystemParameters.MinimumHorizontalDragDistance || Math.Abs(diff.Y) < SystemParameters.MinimumVerticalDragDistance)
+				return;
+
+			var dropList = new System.Collections.Specialized.StringCollection();
+			dropList.Add(dragFile.Path);
+
+			var data = new DataObject();
+			data.SetFileDropList(dropList);
+
+			DragDrop.DoDragDrop(localFilesList, data, DragDropEffects.Copy);
+
+			e.Handled = false;
+		}
+
+		private void localFilesList_PreviewMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+		{
+			if (dragging)
+			{
+				dragging = false;
+				Mouse.Capture(null);
+			}
+		}
+
+		private void remoteFilesList_DragEnter(object sender, DragEventArgs e)
+		{
+			if (!e.Data.GetDataPresent(DataFormats.FileDrop))
+				e.Effects = DragDropEffects.None;
+		}
+
+		private void remoteFilesList_Drop(object sender, DragEventArgs e)
+		{
+			if (!e.Data.GetDataPresent(DataFormats.FileDrop))
+				return;
+
+			var data = e.Data.GetData(DataFormats.FileDrop);
+			var paths = data as IEnumerable<string>;
+			foreach (string path in paths)
+			{
+				uploadFile(path);
+			}
+		}
+
+		void uploadFile(string localPath)
+		{
+			string remotePath = System.IO.Path.GetFileName(localPath);
+			using (var stream = File.OpenRead(localPath))
+			{
+				client.UploadFile(stream, remotePath);
 			}
 		}
 	}
---
+++ Sftp/RemoteDirectory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Renci.SshNet.Sftp;
+
+namespace Sftp
+{
+    public class RemoteDirectory
+    {
+		List<SftpFile> files;
+		public IEnumerable<SftpFile> Files
+		{ get { return files; } }
+
+
+    }
+}
